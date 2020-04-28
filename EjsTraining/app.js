2e2dd4e3d9853e27f5f6e7c187fb8a60b9397d81
eufiee <eufracian@gmail.com>
2020-04-29 00:40:50 +0200

    greetings

--- EjsTraining/app.js
+++ EjsTraining/app.js
@@ -6,6 +6,11 @@ app.get("/", function(req, res){
     res.render("index.ejs");
 })
 
+app.get("/greetings/:visitor", function(req, res){
+    var visitor = req.params.visitor.toUpperCase();
+    res.render("greetings.ejs", {visitor:visitor})
+})
+
 app.listen(3000, function(){
     console.log("Server Running...")
 })
